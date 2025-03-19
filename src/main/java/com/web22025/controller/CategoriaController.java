964fd1a4ead6fadc991fb17d54e58fd9600d7f7a
Fabricio <fundao@gmail.com>
2025-03-18 21:10:45 -0300

    Commit 2

--- src/main/java/com/web22025/controller/CategoriaController.java
+++ src/main/java/com/web22025/controller/CategoriaController.java
@@ -1,15 +1,13 @@
 package com.web22025.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -35,13 +33,13 @@ public class CategoriaController {
 			BindingResult result, RedirectAttributes msg
 			) {
 		if(result.hasErrors()) {
-			msg.addFlashAttribute("erroCadastrar","ERRO");
+			msg.addFlashAttribute("erro","ERRO");
 			return "redirect:/categoria/inserir";
 		}
 		var categoria = new Categoria();
 		BeanUtils.copyProperties(dto, categoria);
 		repository.save(categoria);
-		msg.addFlashAttribute("sucessoCadastrar", "Categoria cadastrar");
+		msg.addFlashAttribute("sucesso", "Categoria cadastrada!");
 		return "redirect:/categoria/inserir";
 	}
 	
@@ -53,4 +51,38 @@ public class CategoriaController {
 		return mv;
 	}
 
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable(value="id") int id) {
+		Optional<Categoria> categoria = repository.findById(id);
+		if (categoria.isEmpty()){
+			return "redirect:/categoria/listar";
+		}
+		repository.deleteById(id);
+		return "redirect:/categoria/listar";
+	}
+
+	@GetMapping("/editar/{id}")
+	public ModelAndView editar(@PathVariable(value="id") int id){
+		ModelAndView mv = new ModelAndView("categoria/editar");
+		Optional<Categoria> categoria =repository.findById(id);
+		mv.addObject("id", categoria.get().getID());
+		mv.addObject("nome", categoria.get().getNome());
+ 		return mv;
+	}
+	@PostMapping("/editar/{id}")
+	public String editado(@ModelAttribute @Valid CategoriaDTO dto,
+						  BindingResult result, RedirectAttributes msg,
+						  @PathVariable(value = "id")int id){
+		if (result.hasErrors()){
+			msg.addFlashAttribute("erro","Erro ao editar");
+			return "redirect:/categoria/listar";
+		}
+		Optional<Categoria> cat = repository.findById(id);
+
+		var categoria = cat.get();
+		BeanUtils.copyProperties(dto, categoria);
+		repository.save(categoria);
+		msg.addFlashAttribute("sucesso", "Categoria editada!");
+		return "redirect:/categoria/listar";
+	}
 }
